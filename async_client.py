b3959b5ad6b379b9d18a8cc79ed90a202ead44d7
Supratim Banerjee <sbanerjee12@gmail.com>
2017-07-28 15:33:56 +0530

    fifth commit

--- async_client.py
+++ async_client.py
@@ -19,16 +19,17 @@ async def main(websites):
         htmls = [await fetch(session, html[0],html[1]) for html in websites]
         mid = datetime.now()
         end = datetime.now()
-        postfetch = (mid-start).total_seconds()
         total = (end-start).total_seconds()
-        for html in htmls:
-            print (html)
-        print (postfetch,total)
+        return (total,len(htmls))
 
 
 checklist = pickle.load( open( "checklist.pkl", "rb" ) )
-checklist = list(pd.unique(checklist))[0:2]
+checklist = list(pd.unique(checklist))[0:5]
 jsondata = [json.dumps({'con': '10000','origin': i[0], 'destination': i[1],'location': i[0],'arratloc': '2017-07-08 17:30:00'}) for i in checklist]
 websites = [['http://localhost:50000/',jsondata[ix]] for ix,i in enumerate(checklist)]
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(websites))
+stats = loop.run_until_complete(main(websites))
+output = open('stats.pkl', 'wb')
+pickle.dump(stats, output)
+output.close()
+print (stats)
